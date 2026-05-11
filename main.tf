8c1335221828e9eea89ef77e93280140d0dd4f30
Ubuntu <ubuntu@ip-172-31-2-92.eu-north-1.compute.internal>
2026-05-11 06:24:05 +0000

    Terraform infrastructure setup

---
+++ main.tf
@@ -0,0 +1,56 @@
+resource "aws_security_group" "web_sg" {
+  name = "web-security-group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "terraform_ec2" {
+  ami           = "ami-0989fb15ce71ba39e"
+  instance_type = "t3.micro"
+
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "Terraform-Server"
+  }
+}
